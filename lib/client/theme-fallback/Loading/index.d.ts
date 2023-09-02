96fae7f346bb126eebbc23109e4000e11c46f68b
ianaio <dev@iana.io>
2023-09-02 17:31:28 +0200

    theme fallbacks

---
+++ lib/client/theme-fallback/Loading/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) IANA SYSTEMS, Inc. and its affiliates.
+ *
+ * This source code is licensed under the IANA SYSTEMS license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+/// <reference types="react" />
+import type { LoadingComponentProps } from 'react-loadable';
+export default function Loading({ error, retry, pastDelay, }: LoadingComponentProps): JSX.Element | null;
+
